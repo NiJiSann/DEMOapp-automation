8f9c3f3b857875b558ca78341081c4507cc45c97
Sanjar Abrorxojaev <sabrorxojaev@gmail.com>
2024-02-24 13:08:08 +0500

    Refactor

--- tests/test_midterm_project/conftest.py
+++ tests/test_midterm_project/conftest.py
@@ -1,14 +1,17 @@
 import pytest
 from appium.options.common import AppiumOptions
 from appium import webdriver
-from helper_methods import set_common_capabilities
 
 
 @pytest.fixture(scope="session")
 def get_driver_with_demo_app():
     driver_opts = AppiumOptions()
-    apk = "C:\\Users\\sabro\\Downloads\\Android-MyDemoAppRN.1.3.0.build-244\\Android-MyDemoAppRN.1.3.0.build-244.apk"
+    apk = r"C:\Repo\DEMOapp-automation\Android-MyDemoAppRN.1.3.0.build-244.apk"
     driver_opts.set_capability("appium:app", apk)
-    set_common_capabilities(driver_opts)
+    driver_opts.set_capability("udid", "emulator-5554")
+    driver_opts.set_capability("appium:automationName", "UiAutomator2")
+    driver_opts.set_capability("appium:platformName", "android")
+    driver_opts.set_capability("appium:deviceName", "Pixel_3a_API_34_extension_7_x86_64")
+    driver_opts.set_capability("browserName", "")
     return webdriver.Remote("http://127.0.0.1:4723", options=driver_opts)
 
